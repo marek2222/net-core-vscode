2f85c0dea157401d7180c87dc069d21f53fbedfa
marek2222 <marekmusial2@o2.pl>
2019-02-12 10:32:48 +0100

    RazorPagesMovie: 2_1:   Add a data model Movie, context: RazorPagesMovieContext

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie
 {
@@ -30,7 +32,9 @@ namespace RazorPagesMovie
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            // sql server
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
