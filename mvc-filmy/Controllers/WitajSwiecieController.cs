e0a485062338e52a6f5e7dca71ae12dddb0cb3fc
marek2222 <marekmusial2@o2.pl>
2019-02-13 13:20:57 +0100

    mvc-filmy: 1_3:   Add a controller

---
+++ mvc-filmy/Controllers/WitajSwiecieController.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_filmy.Controllers
+{
+  public class WitajSwiecieController : Controller
+  {
+    // 
+    // GET: /WitajSwiecie/
+    public string Index()
+    {
+      return "To jest domyślna akcja...";
+    }
+
+    // 
+    // GET: /WitajSwiecie/Witaj?nazwa=marek2222&liczba=3 
+    public string Witaj(string nazwa, int liczba = 1)
+    {
+      return HtmlEncoder.Default.Encode($"Cześć {nazwa}, liczba to: {liczba}");
+    }
+  }
+}
