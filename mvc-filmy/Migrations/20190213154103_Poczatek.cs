042ed3c53f05995bbc939c4c48ee0a6cae8fc9e9
marek2222 <marekmusial2@o2.pl>
2019-02-13 16:42:33 +0100

    mvc-filmy: 3_7:   Migracja: dotnet ef migrations add Poczatek,   dotnet ef database update

---
+++ mvc-filmy/Migrations/20190213154103_Poczatek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mvc_filmy.Migrations
+{
+    public partial class Poczatek : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Film",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Tytul = table.Column<string>(nullable: true),
+                    DataWydania = table.Column<DateTime>(nullable: false),
+                    Gatunek = table.Column<string>(nullable: true),
+                    Cena = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Film", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Film");
+        }
+    }
+}
