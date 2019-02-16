11516b5c4ac743d3d6dace820544d3b1322e5675
marek2222 <marekmusial2@o2.pl>
2019-02-16 11:55:24 +0100

    real-apps: 1_5:   I added required NuGet packages:
    dotnet add package Microsoft.EntityFrameworkCore.SQLServer
    dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
    
    and registered context in Startup.cs

--- real-apps/Models/RealAppContext.cs
+++ real-apps/Models/RealAppContext.cs
@@ -2,13 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace real_apps.Models
 {
-    public class RealAppContext : DbContext
+  public class RealAppContext : DbContext
+  {
+    public RealAppContext(DbContextOptions<RealAppContext> options)
+        : base(options)
     {
-        public RealAppContext (DbContextOptions<RealAppContext> options)
-            : base(options)
-        {
-        }
-
-        //public DbSet<Movie> Movie { get; set; }
     }
+
+    //public DbSet<Movie> Movie { get; set; }
+  }
 }
