b23deb1d1cef4266cd8f647af8e593e5e6d28cb0
marek2222 <marekmusial2@o2.pl>
2019-02-16 14:46:04 +0100

    real-apps: 2_5:   Read Values from AppSettings.json File

--- real-apps/Controllers/DemoController.cs
+++ real-apps/Controllers/DemoController.cs
@@ -1,4 +1,6 @@
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace real_apps.Controllers
 {
@@ -6,6 +8,15 @@ namespace real_apps.Controllers
   {
     public IActionResult Index()
     {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+      var configuration = builder.Build();
+      ViewBag.result1 = configuration["Message"];
+      ViewBag.result2 = configuration["MyConfigs:Config1"];
+      ViewBag.result3 = configuration["MyConfigs:Config2"];
+      ViewBag.result4 = configuration["MyConfigs:Config3"];
+      ViewBag.result5 = configuration["Logging:LogLevel:Default"];
       return View();
     }
   }
