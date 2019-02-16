8be99fb68e624732b8fcf36637f4695e3a390405
marek2222 <marekmusial2@o2.pl>
2019-02-16 18:50:17 +0100

    real-apps: 2_10:   Multiple Submit Buttons

--- real-apps/Controllers/DemoController.cs
+++ real-apps/Controllers/DemoController.cs
@@ -2,11 +2,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace real_apps.Controllers
 {
+  [Route("demo")]
   public class DemoController : Controller
   {
+    [Route("")]
+    [Route("index")]
+    [Route("~/")]
     public IActionResult Index()
     {
       return View();
+    }
+
+    [HttpPost]
+    [Route("Work1")]
+    public IActionResult Work1()
+    {
+      return View("Work1");
+    }
+
+
+    [HttpPost]
+    [Route("Work2")]
+    public IActionResult Work2()
+    {
+      return View("Work2");
+    }
+
+
+    [HttpPost]
+    [Route("Work3")]
+    public IActionResult Work3()
+    {
+      return View("Work3");
     }
   }
 }
