e7e38f3c905f00f90d8a2876b6914d85303fb755
marek2222 <marekmusial2@o2.pl>
2019-02-16 14:32:14 +0100

    real-apps: 2_3:   Pass Object from Controller to View

--- real-apps/Controllers/DemoController.cs
+++ real-apps/Controllers/DemoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using real_apps.Models;
 
 namespace real_apps.Controllers
 {
@@ -6,6 +7,15 @@ namespace real_apps.Controllers
   {
     public IActionResult Index()
     {
+      Product product = new Product()
+      {
+        Id = "p01",
+        Name = "Name 1",
+        Photo = "Thumb1.jpg",
+        Price = 5.5,
+        Quantity = 4
+      };
+      ViewBag.product = product;
       return View();
     }
   }
---
+++ real-apps/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace real_apps.Models
+{
+  public class Product
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Photo { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+  }
+}
