6e0e0aa8d0f8fd114685f6b528c949c69dc2aef7
marek2222 <marekmusial2@o2.pl>
2019-02-16 14:39:46 +0100

    real-apps: 2_4:   Pass Objects List from Controller to View

--- real-apps/Controllers/DemoController.cs
+++ real-apps/Controllers/DemoController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using real_apps.Models;
 
 namespace real_apps.Controllers
 {
@@ -6,6 +8,33 @@ namespace real_apps.Controllers
   {
     public IActionResult Index()
     {
+      List<Product> products = new List<Product>() {
+        new Product()
+        {
+          Id = "p01",
+          Name = "Name 1",
+          Photo = "Thumb1.jpg",
+          Price = 5.5,
+          Quantity = 4
+        },
+        new Product()
+        {
+          Id = "p02",
+          Name = "Name 2",
+          Photo = "Thumb2.jpg",
+          Price = 7,
+          Quantity = 3
+        },
+        new Product()
+        {
+          Id = "p03",
+          Name = "Name 3",
+          Photo = "Thumb3.jpg",
+          Price = 8,
+          Quantity = 6
+        }
+      };
+      ViewBag.products = products;
       return View();
     }
   }
