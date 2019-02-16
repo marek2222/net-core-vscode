9b1b5f6244c1c258049b3cd6f385267ff497df1e
marek2222 <marekmusial2@o2.pl>
2019-02-16 15:01:25 +0100

    real-apps: 2_7:   Use Parameters to Routes

--- real-apps/Controllers/DemoController.cs
+++ real-apps/Controllers/DemoController.cs
@@ -2,11 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace real_apps.Controllers
 {
+  [Route("demo")]
   public class DemoController : Controller
   {
+    [Route("")]
+    [Route("index")]
+    [Route("~/")]
     public IActionResult Index()
     {
       return View();
     }
+
+    [Route("demo2/{id}")]
+    public IActionResult Demo2(int id)
+    {
+      ViewBag.id = id;
+      return View("Demo2");
+    }
+
+    [Route("demo3/{id1}/{id2}")]
+    public IActionResult Demo3(int id1, string id2)
+    {
+      ViewBag.id1 = id1;
+      ViewBag.id2 = id2;
+      return View("Demo3");
+    }
+
   }
 }
