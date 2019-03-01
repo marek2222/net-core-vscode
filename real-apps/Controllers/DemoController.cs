e970ccd615921286e16e81bf59b4534062569ef0
marek2222 <marekmusial2@o2.pl>
2019-03-02 00:47:46 +0100

    real-apps: 2_24:   Ajax

--- real-apps/Controllers/DemoController.cs
+++ real-apps/Controllers/DemoController.cs
@@ -1,12 +1,59 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using real_apps.Models;
 
-namespace real_apps.Controllers
-{
-  public class DemoController : Controller
-  {
-    public IActionResult Index()
-    {
-      return View();
+namespace real_apps.Controllers {
+
+  [Route ("demo")]
+  public class DemoController : Controller {
+    [Route ("")]
+    [Route ("index")]
+    [Route ("~/")]
+    public IActionResult Index () {
+      return View ();
     }
+
+    [Route ("demo1")]
+    public IActionResult Demo1 () {
+      return new JsonResult ("Hello");
+    }
+
+    [Route ("demo2/{fullName}")]
+    public IActionResult Demo2 (string fullName) {
+      return new JsonResult ("Hello " + fullName);
+    }
+
+    [Route ("demo3")]
+    public IActionResult Demo3 () {
+      var product = new Product () {
+        Id = "p01",
+        Name = "name 1",
+        Price = 123
+      };
+      return new JsonResult (product);
+    }
+
+    [Route ("demo4")]
+    public IActionResult Demo4 () {
+      var products = new List<Product> () {
+        new Product () {
+          Id = "p01",
+          Name = "name 1",
+          Price = 123
+        },
+        new Product () {
+          Id = "p02",
+          Name = "name 2",
+          Price = 456
+        },
+        new Product () {
+          Id = "p03",
+          Name = "name 3",
+          Price = 789
+        }
+      };
+      return new JsonResult (products);
+    }
+
   }
 }
--- real-apps/Startup.cs
+++ real-apps/Startup.cs
@@ -12,57 +12,46 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using real_apps.Models;
 
-namespace real_apps
-{
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
-    {
+namespace real_apps {
+  public class Startup {
+    public Startup (IConfiguration configuration) {
       Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.Configure<CookiePolicyOptions>(options =>
-      {
+    public void ConfigureServices (IServiceCollection services) {
+      services.Configure<CookiePolicyOptions> (options => {
         // This lambda determines whether user consent for non-essential cookies is needed for a given request.
         options.CheckConsentNeeded = context => true;
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
 
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
 
-      services.AddDbContext<RealAppContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddDbContext<RealAppContext> (options =>
+        options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
     }
 
-
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
+    public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+      if (env.IsDevelopment ()) {
+        app.UseDeveloperExceptionPage ();
+      } else {
+        app.UseExceptionHandler ("/Home/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
+        app.UseHsts ();
       }
 
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-      app.UseCookiePolicy();
+      app.UseHttpsRedirection ();
+      app.UseStaticFiles ();
+      app.UseCookiePolicy ();
 
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-                  name: "default",
-                  template: "{controller=Demo}/{action=Index}/{id?}");
+      app.UseMvc (routes => {
+        routes.MapRoute (
+          name: "default",
+          template: "{controller=Demo}/{action=Index}/{id?}");
       });
     }
   }
