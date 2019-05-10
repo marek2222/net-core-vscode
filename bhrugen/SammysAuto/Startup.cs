32cd0aba7332a0e64e671ff3c2e20d554dcca2a1
marek2222 <marekmusial2@o2.pl>
2019-05-10 14:17:34 +0200

    4.38:  Add note to appsettings.json   and  Startup.cs.
    Add DefaultConnection to appsettings.json file:
        "ConnectionStrings": {
          "DefaultConnection": "Server=localhost\\SQLEXPRESS;Database=net-core-test;Integrated security=True; Trusted_Connection=True;"
        },
    
      Change UseSqlite on UseSqlServer  in Startup.cs;
        services.AddDbContext<ApplicationDbContext> (options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
        services.AddDbContext<ApplicationDbContext> (options => options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));

--- bhrugen/SammysAuto/Startup.cs
+++ bhrugen/SammysAuto/Startup.cs
@@ -3,72 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SammysAuto.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SammysAuto.Data;
 
-namespace SammysAuto
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+namespace SammysAuto {
+	public class Startup {
+		public Startup (IConfiguration configuration) {
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices (IServiceCollection services) {
+			services.Configure<CookiePolicyOptions> (options => {
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded 		= context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+			services.AddDbContext<ApplicationDbContext> (options =>
+				options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+			services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext> ();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+			services.AddMvc().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
+		}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+			if (env.IsDevelopment ()) {
+				app.UseDeveloperExceptionPage ();
+				app.UseDatabaseErrorPage ();
+			} else {
+				app.UseExceptionHandler ("/Home/Error");
+				app.UseHsts ();
+			}
 
-            app.UseAuthentication();
+			app.UseHttpsRedirection ();
+			app.UseStaticFiles ();
+			app.UseCookiePolicy ();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseAuthentication ();
+
+			app.UseMvc (routes => {
+				routes.MapRoute (
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
