076b18083a1268497a0e8ad0d2169240d44b05d8
marek2222 <marekmusial2@o2.pl>
2019-05-10 15:44:58 +0200

    Scaffold identity
        $ dotnet aspnet-codegenerator identity -dc SammysAuto.Data.ApplicationDbContext
          --force    // Use '--force' to overwrite: wwwroot/css/site.css
          // --files "Account.Register;Account.Login;Account.Logout"

--- bhrugen/SammysAuto/Startup.cs
+++ bhrugen/SammysAuto/Startup.cs
@@ -24,17 +24,46 @@ namespace SammysAuto {
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices (IServiceCollection services) {
 			services.Configure<CookiePolicyOptions> (options => {
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-				options.CheckConsentNeeded 		= context => true;
+				options.CheckConsentNeeded = context => true;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
 
 			services.AddDbContext<ApplicationDbContext> (options =>
 				options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
 
-			services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext> ();
+			services.AddDefaultIdentity<IdentityUser> ().AddEntityFrameworkStores<ApplicationDbContext> (); //.AddDefaultUI(UIFramework.Bootstrap4)
 
-			services.AddMvc().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
+			services.Configure<IdentityOptions> (options => {
+				// Password settings.
+				options.Password.RequireDigit = true;
+				options.Password.RequireLowercase = true;
+				options.Password.RequireNonAlphanumeric = true;
+				options.Password.RequireUppercase = true;
+				options.Password.RequiredLength = 6;
+				options.Password.RequiredUniqueChars = 1;
+
+				// Lockout settings.
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes (5);
+				options.Lockout.MaxFailedAccessAttempts = 5;
+				options.Lockout.AllowedForNewUsers = true;
+
+				// User settings.
+				options.User.AllowedUserNameCharacters =
+					"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+				options.User.RequireUniqueEmail = false;
+			});
+
+			services.ConfigureApplicationCookie (options => {
+				// Cookie settings
+				options.Cookie.HttpOnly = true;
+				options.ExpireTimeSpan = TimeSpan.FromMinutes (5);
+
+				options.LoginPath = "/Identity/Account/Login";
+				options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+				options.SlidingExpiration = true;
+			});
+
+			services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
