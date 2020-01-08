15afcd9a4379fa9d4bbafe1529a903f65f6073a4
marek2222 <marekmusial2@o2.pl>
2020-01-08 06:52:20 +0100

    ZIP:  01.  New Console Project - dotnet new console

---
+++ zip_extract_compress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zip_extract_compress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
